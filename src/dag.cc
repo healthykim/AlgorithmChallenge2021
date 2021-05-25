3d648bd04123835fca1d86fac7e91317757fa19c
user.email <bsbs8645@snu.ac.kr>
2021-05-26 05:55:11 +0900

    DAG

---
+++ src/dag.cc
@@ -0,0 +1,18 @@
+//
+// Created by 문보설 on 2021/05/25.
+//
+
+#include "dag.h"
+
+Dag::Dag(const std::string& filename, const CandidateSet &candidateSet, bool is_query) : Graph(filename, candidateSet, is_query)
+{
+};
+Dag::~Dag() {}
+
+
+
+
+
+
+
+
